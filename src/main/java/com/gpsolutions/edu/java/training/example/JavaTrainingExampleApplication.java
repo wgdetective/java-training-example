837c695087e9548187c93f86fba6684694f3f03f
Wladimir Litvinov <w.litvinov@gpsolutions.com>
2020-01-21 18:10:05 +0300

    init commit

---
+++ src/main/java/com/gpsolutions/edu/java/training/example/JavaTrainingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gpsolutions.edu.java.training.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpsolutions/edu/java/training/example/JavaTrainingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpsolutions.edu.java.training.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTrainingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
